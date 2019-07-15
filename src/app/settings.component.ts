7bc20d8b0981bb42cab962c943ef7480294be668
Noah Roston <rostonn@gmail.com>
2019-07-14 21:54:24 -0400

    showing loading and enter input for name

--- src/app/settings.component.ts
+++ src/app/settings.component.ts
@@ -6,19 +6,25 @@ import { NameService } from './name.service';
 
 @Component({
   template: `
-    <input [(ngModel)]="name"/>
+    <input [(ngModel)]="name" (click)="name=''" (keyup)="enterSetName($event)"/>
     <button (click)="setName()">Set Name</button>
-    {{name}}
     <button (click)="getName()">Show Name</button>
-
     `
 })
 export class SettingsComponent {
     name = '';
 
     constructor(private nameService: NameService){}
+
+    enterSetName(event: any) {
+        if(event.key === "Enter") {
+            this.setName();
+        }
+    }
+
     setName(){
         this.nameService.setName(this.name);
+        alert('Your name is ' + this.name);
     }
     getName(){
         let x = this.nameService.getName();
